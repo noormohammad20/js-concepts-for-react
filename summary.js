826f5fc03f9b2bcb629f3259bf70f50578118e80
noormohammad20 <mdsaim204@gmail.com>
2022-03-08 17:54:41 +0600

    list of js for react

---
+++ summary.js
@@ -0,0 +1,55 @@
+/* 
+Fundamentals
+
+1.1 variable (let,const) three types of variable
+1.2 condition (<,>,<=,>=,===,!==)&& || if- else-if - else
+1.3 array: declare,length,index,push,pop,indexOf,includes
+1.4 for loop, while
+1.5 function : declare,return,parameter
+1.6 object property including, array , object
+
+-----------------------------------
+ES6
+1. Template String ${}
+2. Spread (...) 
+2.1 copy an array then add a new element to an array .
+2.2 use filter remove an element form an array. 
+3. Arrow function : no parameter, single parameter, multiple parameter,multiline arrow function. 
+4. destructuring : object destructuring and array destructuring .
+5. object shorthand
+6. function : parameter default value .
+7. optional chaining (?.)
+--------------------------------------
+Browser API
+1. local storage session storage
+2. location API
+3. history API
+4. fetch
+-------------------------------
+Others
+1. Array: map,foreach,filter,find
+2. ternary operator 
+3. logical && and logical || 
+4. json: stringify , parse
+
+
+*/
+
+const x = 50
+const y = 100
+
+const obj = {
+
+    x: x,
+    y: y
+}
+// shortcut
+const obj1 = { x, y }
+
+const name = 'solimullah'
+const location = 'dhaka'
+const nobab = {
+    name: name,
+    location: location
+}
+const nobab1 = { name, location }
